ca7510867603ad80b319f34bdeb6feb397fd0812
Keagan Chisnall <keagan@chisnall.io>
2021-12-06 18:48:39 +1000

    Switched demo navigation to select

--- demo/src/components/Index.vue
+++ demo/src/components/Index.vue
@@ -1,11 +1,15 @@
 <template>
+  <h1 class="instructions">↑↑↑ Select a component ↑↑↑</h1>
   <div class="logo">
     <img src="../assets/v3-gmaps.png" />
-    <h1>v3-gmaps</h1>
+    <h1>v3-gmaps demo</h1>
   </div>
 </template>
 
 <style lang="scss">
+.instructions {
+  text-align: center;
+}
 .logo {
   margin-top: 100px;
   font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
--- demo/src/components/Navigation.vue
+++ demo/src/components/Navigation.vue
@@ -4,7 +4,10 @@
       <router-link to="/" exact><img src="../assets/v3-gmaps.png" /></router-link>
     </div>
     <div class="links">
-      <router-link v-for="route in menuRoutes" :to="route.path" :key="route.name" exact>{{ route.name }}</router-link>
+      <label for="navigation-select">Component:</label>
+      <select id="navigation-select" @change="gotoRoute" :value="selRoute">
+        <option v-for="route in menuRoutes" :key="route.name" :value="route.path">{{ route.name }}</option>
+      </select>
     </div>
     <div class="github">
       <a href="https://github.com/xon52/v3-gmaps" target="_blank" exact><img src="../assets/github.png" /></a>
@@ -13,7 +16,16 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
 import { menuRoutes } from '../router'
+const router = useRouter()
+const selRoute = computed(() => {
+  const route = router.currentRoute.value.path
+  if (menuRoutes.some((e) => e.path === route)) return route
+  return null
+})
+const gotoRoute = (e: Event) => router.push((e.target as HTMLSelectElement).value)
 </script>
 
 <style lang="scss">
@@ -36,26 +48,17 @@ nav {
     }
   }
   .links {
+    font-size: larger;
     margin: 0 20px;
-    a {
-      display: inline-block;
-      color: #eee;
-      font-size: 1.2rem;
-      height: 40px;
-      line-height: 40px;
-      min-width: 100px;
-      padding: 0 20px;
-      width: auto;
-      text-decoration: none;
-      transition: 0.3s background-color;
+    label {
+      margin-right: 10px;
     }
-    a:hover {
-      background-color: #44a5ff;
-    }
-    a.router-link-active {
-      background-color: #eee;
-      color: #44a5ff;
-      cursor: default;
+    select {
+      font-size: large;
+      padding: 10px;
+      color: #444;
+      min-width: 300px;
+      height: 100%;
     }
   }
 }
