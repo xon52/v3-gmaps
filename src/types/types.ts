00f74d82e29a48994ccc60557aee0d554340665e
Keagan Chisnall <keagan@chisnall.io>
2022-06-18 06:48:53 +1000

    Added GmapsControlPosition type

--- src/types/types.ts
+++ src/types/types.ts
@@ -49,6 +49,23 @@ type GmapsMapTypeStyle = {
 }
 
 /**
+ * https://developers.google.com/maps/documentation/javascript/reference/control#ControlPosition
+ */
+export type GmapsControlPosition =
+  | 'BOTTOM_CENTER'
+  | 'BOTTOM_LEFT'
+  | 'BOTTOM_RIGHT'
+  | 'LEFT_BOTTOM'
+  | 'LEFT_CENTER'
+  | 'LEFT_TOP'
+  | 'RIGHT_BOTTOM'
+  | 'RIGHT_CENTER'
+  | 'RIGHT_TOP'
+  | 'TOP_CENTER'
+  | 'TOP_LEFT'
+  | 'TOP_RIGHT'
+
+/**
  * https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLng
  */
 export type GmapsPosition = {
