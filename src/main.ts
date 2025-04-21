85dfe94ae91892ac7ee2e93544df6c745964d029
Keagan Chisnall <keagan@chisnall.io>
2025-04-21 16:15:21 +1000

    Checkpoint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import gmaps from 'v3-gmaps'
-import 'v3-gmaps/dist/style.css'
-import router from './router'
+import gmaps from 'v3-gmaps';
+import router from './router';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 createApp(App)
-  .use(router)
-  .use(gmaps, { key: import.meta.env.VITE_GOOGLE_KEY, libraries: ['visualization'] })
-  .mount('#app')
+	.use(router)
+	.use(gmaps, { key: import.meta.env.VITE_GOOGLE_KEY, libraries: ['visualization'] })
+	.mount('#app');
